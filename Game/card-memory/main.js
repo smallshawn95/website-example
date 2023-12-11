098e5043999273213c3260d751c91ec3f735aba2
smallshawn95 <smallshawn95@gmail.com>
2023-12-12 00:30:52 +0800

    2023-12-12 update card-memory (#38)

--- Game/card-memory/main.js
+++ Game/card-memory/main.js
@@ -13,6 +13,8 @@ var card_emojis = emojis.sort(function(){
     }
 });
 
+let count = 0;
+
 for(var i = 0; i < 16; i++){
     let box = document.createElement("div");
     box.className = "item";
@@ -20,23 +22,24 @@ for(var i = 0; i < 16; i++){
     box.onclick = function(){
         this.classList.add("boxOpen");
         setTimeout(function(){
-            if(document.querySelectorAll(".boxOpen").length == 2){
-                let a = document.querySelectorAll(".boxOpen")[0]; 
+            if(document.querySelectorAll(".boxOpen").length > 1){
+                count += 1;
+                let a = document.querySelectorAll(".boxOpen")[0];
                 let b = document.querySelectorAll(".boxOpen")[1];
                 if(a.innerHTML == b.innerHTML){
                     a.classList.add("boxMatch");
                     b.classList.add("boxMatch");
                     a.classList.remove("boxOpen");
                     b.classList.remove("boxOpen");
-                    if(document.querySelectorAll("boxMatch").length == 16){
-                        alert("Win");
+                    if(document.querySelectorAll(".boxMatch").length == 16){
+                        alert(`Win, you use ${count} times in total.`);
                     }
                 }else{
                     a.classList.remove("boxOpen");
                     b.classList.remove("boxOpen");
                 }
             }
-        }, 500);
+        }, 800);
     }
     document.querySelector(".game").appendChild(box);
 }
