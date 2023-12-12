50df479132f5aaee0f73983b4e21f9dcbb6ab0ee
smallshawn95 <smallshawn95@gmail.com>
2023-12-12 13:04:04 +0800

    2023-12-12 add random-background-color (#42)

---
+++ Animation/random-background-color/main.js
@@ -0,0 +1,8 @@
+function changeBackgroundColor(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+setInterval(changeBackgroundColor, 1000);
